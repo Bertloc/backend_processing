34ee582d3ae2c823160726a4537e93f934223cd2
Humberto <raydekile@gmail.com>
2025-01-08 13:16:16 -0600

    ultima version equisde

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from app import create_app
+from waitress import serve
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
-
-
+    # Waitress es un servidor WSGI recomendado para producción.
+    serve(app, host="0.0.0.0", port=5000)

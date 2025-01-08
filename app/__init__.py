714915a500c14161635d754475568655d167e829
Humberto <raydekile@gmail.com>
2025-01-08 10:46:06 -0600

    recepcion desde produccion

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,8 @@ from flask_cors import CORS  # Importa CORS
 def create_app():
     app = Flask(__name__)
     
-    # Habilitar CORS para toda la aplicación
-    CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
+    # Permitir CORS desde localhost y Railway
+    CORS(app, resources={r"/*": {"origins": ["http://localhost:3000", "https://backendprocessing-production.up.railway.app"]}})
 
     # Registrar las rutas
     from app.routes import api

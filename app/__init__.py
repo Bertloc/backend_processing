b07c48d6d366a3fecdecf1f85ce782e07908e2d2
Humberto <raydekile@gmail.com>
2025-01-08 14:28:12 -0600

    unodos

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,15 @@
 from flask import Flask
-from flask_cors import CORS  # Importa CORS
+from flask_cors import CORS  
 
 def create_app():
     app = Flask(__name__)
     
-    # Permitir CORS desde localhost y Railway
-    CORS(app, resources={r"/*": {"origins": ["http://localhost:3000", "https://backendprocessing-production.up.railway.app"]}})
-
-    # Registrar las rutas
+    # Configuración CORS para permitir ambos entornos
+    CORS(app, resources={r"/*": {"origins": ["http://localhost:3000", "https://backendprocessing-production.up.railway.app"]}}, supports_credentials=True)
+    
+    # Registrar rutas
     from app.routes import api
     app.register_blueprint(api, url_prefix="/api")
 
     return app
+
--- run.py
+++ run.py
@@ -1,8 +1,10 @@
+import os
 from app import create_app
 from waitress import serve
 
 app = create_app()
 
 if __name__ == "__main__":
-    # Waitress es un servidor WSGI recomendado para producción.
-    serve(app, host="0.0.0.0", port=5000)
+    # Usando el puerto de la variable de entorno de Railway
+    port = int(os.environ.get("PORT", 5000))  # $PORT dinámico o 5000 por defecto
+    serve(app, host="0.0.0.0", port=port)

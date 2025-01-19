5e86a22cf1aa5fa324232688503700cef5dec9b0
Humberto <raydekile@gmail.com>
2025-01-19 00:29:49 -0600

    restauracion leve

--- run.py
+++ run.py
@@ -1,10 +1,8 @@
-import os
 from app import create_app
-from waitress import serve
 
 app = create_app()
 
 if __name__ == "__main__":
-    # Usando el puerto de la variable de entorno de Railway
-    port = int(os.environ.get("PORT", 5000))  # $PORT dinámico o 5000 por defecto
-    serve(app, host="0.0.0.0", port=port)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
+

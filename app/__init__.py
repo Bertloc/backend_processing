ba91717849d6894c47f4b6252a150b9120d48bbf
Paracetamol21 <alonso23dieg@gmail.com>
2025-01-19 03:16:00 -0600

    Actualizar URL de CORS para Render

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,14 @@
 from flask import Flask
-from flask_cors import CORS  
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
-    
+
     # Configuración CORS para permitir ambos entornos
-    CORS(app, resources={r"/*": {"origins": ["http://localhost:3000", "https://backendprocessing-production.up.railway.app"]}}, supports_credentials=True)
-    
+    CORS(app, resources={r"/*": {"origins": ["http://localhost:3000", "https://backend-processing.onrender.com"]}}, supports_credentials=True)
+
     # Registrar rutas
     from app.routes import api
     app.register_blueprint(api, url_prefix="/api")
 
     return app
-
